56dda8b6a47f0ba71d9814dee7f4616f5c9c61bf
Nikhil Nishad <rrnrrn007@gmail.com>
2026-02-13 14:53:26 +0530

    all

--- netcrud/Data/ProductRepository.cs
+++ netcrud/Data/ProductRepository.cs
@@ -8,6 +8,9 @@ namespace netcrud.Data;
 public interface IProductRepository
 {
     Task<IEnumerable<Product>> GetAllAsync();
+
+
+
     Task<Product?> GetByIdAsync(int id);
     Task<Product> CreateAsync(Product product);
     Task<bool> UpdateAsync(Product product);
